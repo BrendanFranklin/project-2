718b5d64f1a4ea077eb3e2f015a1f07c524090a2
Michael Perez <michaelperezmath@gmail.com>
2019-02-05 19:27:27 -0500

    Added basic JWT stuff, pojos, and Hibernate annotations

---
+++ project2/project2/src/main/java/org/project2/pojos/Role.java
@@ -0,0 +1,39 @@
+package org.project2.pojos;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Roles", schema = "Pretense")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer id;
+    
+    @Column(name = "role_name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ project2/project2/src/main/java/org/project2/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package org.project2.security;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 1_800_000; // 30 minutes
+    public static final String SECRET = "Banana_Town_Bois";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String PREFIX = "Bearer ";
+}
