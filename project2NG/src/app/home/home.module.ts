402033ffbe0a2c720eddd6d3ea5492b028de8a1c
Matthew <hishmars@gmail.com>
2019-02-05 20:46:50 -0500

    Added login information, Environment variables are placeholder for now

--- project2NG/src/app/home/home.module.ts
+++ project2NG/src/app/home/home.module.ts
@@ -5,16 +5,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProspectivepageComponent } from './prospectivepage/prospectivepage.component';
-import { AppartmentlistComponent } from './prospectivepage/appartmentlist/appartmentlist.component';
 import { FilterComponent } from './prospectivepage/components/filter/filter.component';
-import { ResidentloginComponent } from './residentlogin/residentlogin.component';
-import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
 import { EmployeeLoginPageComponent } from './employee-login-page/employee-login-page.component';
 import { ResidentLoginPageComponent } from './resident-login-page/resident-login-page.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
+import { LoginComponent } from './login-component/login-component.component';
 
 @NgModule({
-  declarations: [HomepageComponent, NavbarComponent, ProspectivepageComponent, AppartmentlistComponent, FilterComponent, ResidentloginComponent, EmployeeloginComponent, EmployeeLoginPageComponent, ResidentLoginPageComponent, LoginComponentComponent],
+  declarations: [HomepageComponent, NavbarComponent, ProspectivepageComponent, FilterComponent, EmployeeLoginPageComponent, ResidentLoginPageComponent, LoginComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
--- project2NG/src/app/home/login-component/login-component.component.ts
+++ project2NG/src/app/home/login-component/login-component.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login-service.service';
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
   styleUrls: ['./login-component.component.css']
 })
-export class LoginComponentComponent implements OnInit {
+export class LoginComponent implements OnInit {
+  shake: boolean;
+  username: string;
+  password: string;
+  locationUrl: string;
 
-  constructor() { }
+   constructor(private router: Router,
+    private login: LoginService
+    ) { }
 
   ngOnInit() {
+    this.shake = false;
   }
 
-}
+  submit() {
+     this.login.authenticate(this.username, this.password,
+     () => this.router.navigate([this.locationUrl]),
+     (err) => {
+       console.log(err);
+       this.shake=true;
+       this.username="";
+       this.password="";
+       //this.location.go(`${environment.failLoginUrl}?status=${err.status}&msg=${err.error}`)
+     });
+  }
+  noShake(){
+    this.shake=false;
+  }
+ }
+
