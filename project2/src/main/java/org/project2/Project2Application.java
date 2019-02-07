c6699260fabc917b3575d4d0030e809710e8d696
Michael Perez <michaelperezmath@gmail.com>
2019-02-06 20:16:22 -0500

    Some JWTs and Hibernate stuff

--- project2/src/main/java/org/project2/Project2Application.java
+++ project2/src/main/java/org/project2/Project2Application.java
@@ -2,9 +2,20 @@ package org.project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class Project2Application {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
