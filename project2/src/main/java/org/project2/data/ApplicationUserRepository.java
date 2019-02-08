8175fdde4f82a0a55c0f97418e92f456cd37c6b5
Michael Perez <michaelperezmath@gmail.com>
2019-02-08 14:33:17 -0500

    Nothing worked so we have to start anew

--- project2/src/main/java/org/project2/data/ApplicationUserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.project2.data;
-
-import org.project2.pojos.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationUserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
