6be25f9adbdbf951c586c519f1f87dc11f7a8bb6
Michael Perez <michaelperezmath@gmail.com>
2019-02-08 14:35:23 -0500

    Fixing Test so the Java merges don't return our bad files

--- project2/src/main/java/org/project2/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package org.project2.security;
-
-public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 14_400_000; // 4 hours
-    public static final String SECRET = "Banana_Town_Bois";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String PREFIX = "Bearer ";
-}
