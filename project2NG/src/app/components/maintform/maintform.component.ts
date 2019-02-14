d86096f1e30509538add9430d98be7d50b3fd35e
Brendan Franklin <quitefranklin@gmail.com>
2019-02-14 17:52:33 -0500

    up to date

--- project2NG/src/app/components/maintform/maintform.component.ts
+++ project2NG/src/app/components/maintform/maintform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ticket} from '../../models/ticket';
 
 
 @Component({
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintformComponent implements OnInit {
 
-  constructor() { }
+  ticket: Ticket;
+
+  constructor() {
+  }
 
   ngOnInit() {
+
   }
 
 }
