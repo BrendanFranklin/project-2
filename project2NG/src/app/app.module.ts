9a80fac417c0d960c76aa6bfe6ffae937fe7ec76
Brendan Franklin <quitefranklin@gmail.com>
2019-02-14 15:03:09 -0500

    list components added

--- project2NG/src/app/app.module.ts
+++ project2NG/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{ FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
 
 
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- project2NG/src/app/global/global.module.ts
+++ project2NG/src/app/global/global.module.ts
@@ -4,10 +4,16 @@ import {TicketlistComponent} from '../components/ticketlist/ticketlist.component
 import { LogoutbarComponent } from '../components/logoutbar/logoutbar.component';
 import { DetailsmodalComponent } from '../components/detailsmodal/detailsmodal.component';
 import {UserlistComponent} from '../components/userlist/userlist.component';
+import {ApartmentlistComponent} from '../components/apartmentlist/apartmentlist.component';
+import {RentlistComponent} from '../components/rentlist/rentlist.component';
+import {ApplicationlistComponent} from '../components/applicationlist/applicationlist.component';
 
 
 @NgModule({
   declarations: [
+      ApplicationlistComponent,
+      ApartmentlistComponent,
+      RentlistComponent,
       UserlistComponent,
       TicketlistComponent,
   LogoutbarComponent,
@@ -16,6 +22,9 @@ import {UserlistComponent} from '../components/userlist/userlist.component';
     CommonModule
   ],
   exports: [
+      ApplicationlistComponent,
+      ApartmentlistComponent,
+      RentlistComponent,
       UserlistComponent,
       TicketlistComponent,
       LogoutbarComponent,
