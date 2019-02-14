5b382e130cfc2f106d2129796ca791f3b0142fc8
Matthew <hishmars@gmail.com>
2019-02-13 21:19:32 -0500

    added more environment stuff and hooked in more homepage stuff

--- project2NG/src/app/global/global.module.ts
+++ project2NG/src/app/global/global.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TicketlistComponent} from '../components/ticketlist/ticketlist.component';
+import { LogoutbarComponent } from '../components/logoutbar/logoutbar.component';
+import { DetailsmodalComponent } from '../components/detailsmodal/detailsmodal.component';
 
 @NgModule({
-  declarations: [TicketlistComponent],
+  declarations: [TicketlistComponent,
+  LogoutbarComponent],
   imports: [
     CommonModule
   ],
   exports: [
-      TicketlistComponent
+      TicketlistComponent,
+      LogoutbarComponent,
+      DetailsmodalComponent
   ]
 })
 export class GlobalModule { }
